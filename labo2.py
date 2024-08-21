0ae967d665d0429128b6196da1ffaeddd8b49f08
YANG LI <yang.gomez@correounivalle.edu.co>
2024-08-21 11:17:00 -0500

    labo2.py

--- labo2.py
+++ labo2.py
@@ -1,13 +1,16 @@
 class Bicletas:
-    def __init__(self, marca, modelo):
+    def __init__(self, marca, modelo, color):
         self.marca = marca 
         self.modelo = modelo 
+        self.color = color
         
-trekMadone = Bicletas("trek", "trekMadone")
-trek23 = Bicletas("trek", "trek23")
+trekMadone = Bicletas("trek", "trekMadone","azul turquesa")
+trek23 = Bicletas("trek", "trek23", "verde azulado")
+
 
 print(type(trekMadone))
 print(type(trek23))
 
-print(trekMadone.marca, trekMadone.modelo )
-print(trek23.marca, trek23.modelo)
+
+print(trekMadone.marca, trekMadone.modelo, trekMadone.color )
+print(trek23.marca, trek23.modelo, trek23.color)
